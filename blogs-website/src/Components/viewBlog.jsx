041d06e9822dadf76180fb2c0dd8129fda43716c
HamzaRasheed26 <hamzarasheed19961@gmail.com>
2023-07-30 00:00:44 +0500

    view Blog Update

--- blogs-website/src/Components/viewBlog.jsx
+++ blogs-website/src/Components/viewBlog.jsx
@@ -4,14 +4,12 @@ import { useParams } from "react-router-dom";
 export default function ViewBlog() {
   const { id } = useParams(); // Access the 'id' parameter from the URL
 
-  const [blogs, setBlogs] = useState([]);
   const [blog, setBlog] = useState({});
 
   useEffect(() => {
     async function getData() {
       const response = await fetch("http://localhost:4000/Blogs");
       const data = await response.json();
-      setBlogs(data);
 
       // Find the selected blog by its id
       const selectedBlog = data.find((blog) => blog.Id === parseInt(id, 10));
@@ -33,6 +31,10 @@ export default function ViewBlog() {
   return (
     <div className="blog-details">
       <div className="details">
+        <img
+          src={`http://localhost:4000/images/${blog.coverImage}`}
+          alt="Cover Image..."
+        />
         <h1>{blog.title}</h1>
         <p>
           Author: <span> {blog.author}</span>
